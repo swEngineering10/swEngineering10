92e18a81195d642929af57b375170b07bd565e5a
minyeongg <126530262+minyeongg@users.noreply.github.com>
2023-03-29 17:21:42 +0900

    2nd file structure
    2nd file structure

---
+++ UNO_Game/classes/enums/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+from pygame.color import Color
+
+class Colors(Enum):
+    Black = Color('Black')
+    BLUE = Color((85, 85, 255))
+    YELLOW = Color((255, 170, 0))
+    RED = Color((255, 85, 85))
+    GREEN = Color((0, 170, 0))
