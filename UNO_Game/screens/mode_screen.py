d747e8fbfd908545aca4935e65f0678a96f86202
sohxxny <sohxxny@gmail.com>
2023-04-27 21:58:09 +0900

    파일 복구

--- UNO_Game/screens/mode_screen.py
+++ UNO_Game/screens/mode_screen.py
@@ -12,7 +12,6 @@ from screens.abc_screen import Screen
 from screens.lobby_screen import LobbyScreen
 from screens.map_screen import MapScreen
 
-
 class ModeScreen(Screen):
     def __init__(self, surface: Surface, manager: pygame_gui.UIManager, networking: Networking):
         super().__init__(surface, manager, networking)
@@ -24,17 +23,10 @@ class ModeScreen(Screen):
         self.screen = pygame.display.set_mode((WINDOW_SIZE))
         self.screen_width, self.screen_height = WINDOW_SIZE
 
-        with open('keys.json', 'r') as f:
-            keyboard_data = json.load(f)
-
-        self.key_up = keyboard_data["keyboard"]["up"]
-        self.key_down = keyboard_data["keyboard"]["down"]
-
         # Select Mode 텍스트
         self.font = pygame.font.SysFont(None, 80)
         self.text = self.font.render("Select Mode", True, (255, 255, 255))
-        self.text_rect = self.text.get_rect(
-            center=(self.screen_width//2, self.screen_height*0.3))
+        self.text_rect = self.text.get_rect(center=(self.screen_width//2, self.screen_height*0.3))
 
         # 버튼 생성
         self.normal_mode_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
@@ -42,40 +34,8 @@ class ModeScreen(Screen):
         self.story_mode_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
             self.screen_width // 2 - 100, self.screen_height // 2 * 1.3, 200, 50), text='STORY MODE', manager=manager)
 
-        buttons = [self.normal_mode_button, self.story_mode_button]
-
-        self.buttons = buttons
-        self.selected_button_index = 0
-        self.buttons[self.selected_button_index]._set_active()
-
         # 이벤트 처리 함수
     def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == self.key_up:
-                self.selected_button_index = (
-                    self.selected_button_index - 1) % len(self.buttons)
-                self.buttons[self.selected_button_index]._set_active()
-                self.buttons[(self.selected_button_index + 1) %
-                             len(self.buttons)].unselect()
-                self.buttons[(self.selected_button_index + 1) %
-                             len(self.buttons)].enable()
-            elif event.key == self.key_down:
-                self.selected_button_index = (
-                    self.selected_button_index + 1) % len(self.buttons)
-                self.buttons[(self.selected_button_index) %
-                             len(self.buttons)]._set_active()
-                self.buttons[(self.selected_button_index - 1) %
-                             len(self.buttons)].unselect()
-                self.buttons[(self.selected_button_index - 1) %
-                             len(self.buttons)].enable()
-            elif event.key == pygame.K_RETURN:
-                clicked_button = self.buttons[self.selected_button_index]
-                if clicked_button == self.normal_mode_button:
-                    self.next_screen = LobbyScreen
-                    self.is_running = False
-                elif clicked_button == self.story_mode_button:
-                    self.next_screen = MapScreen
-                    self.is_running = False
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             if event.ui_element == self.normal_mode_button:
                 self.next_screen = LobbyScreen
@@ -84,10 +44,10 @@ class ModeScreen(Screen):
                 self.next_screen = MapScreen
                 self.is_running = False
 
+
          # run 함수
-
     def run(self, events: list[Event]) -> bool:
-
+        
         self.screen.blit(self.background, (0, 0))
         self.screen.blit(self.text, self.text_rect)
 
---
+++ UNO_Game/screens/volume_screen.py
@@ -0,0 +1,97 @@
+import pygame
+import pygame.freetype
+import pygame_gui
+import json
+from pygame.surface import Surface
+from pygame.event import Event
+from pygame_gui.elements.ui_button import UIButton
+
+from utility import resolution
+from client.networking import Networking
+from screens.abc_screen import Screen
+
+class VolumeScreen(Screen):
+    def __init__(self, surface: Surface, manager: pygame_gui.UIManager, networking: Networking):
+        super().__init__(surface, manager, networking)
+
+        self.screen_width, self.screen_height = resolution()
+        WINDOW_SIZE = (self.screen_width, self.screen_height)
+
+        self.background = pygame.Surface(WINDOW_SIZE)
+        self.screen = pygame.display.set_mode((WINDOW_SIZE))
+        self.screen_width, self.screen_height = WINDOW_SIZE
+
+        self.font = pygame.font.SysFont(None, 100)
+        self.uni_font = pygame.font.SysFont(None, 30)
+
+        # "Volume" 텍스트 생성
+        self.text = self.font.render("VOLUME", True, (255, 255, 255))
+        self.text1 = self.uni_font.render("Static Sound", True, (255, 255, 255))
+        self.text2 = self.uni_font.render("Background", True, (255, 255, 255))
+        self.text3 = self.uni_font.render("Sound Effect", True, (255, 255, 255))
+
+        # 텍스트의 중심 좌표 계산
+        self.text_rect = self.text.get_rect(center=(self.screen_width//2, self.screen_height//2 * 0.5))
+        self.text1_rect = self.text1.get_rect(
+            center=(self.screen_width // 2 * 0.5, self.screen_height // 2))
+        self.text2_rect = self.text2.get_rect(
+            center=(self.screen_width // 2 * 0.5, self.screen_height // 2 * 1.3))
+        self.text3_rect = self.text3.get_rect(
+            center=(self.screen_width // 2 * 0.5, self.screen_height // 2 * 1.6))
+
+        self.slider1 = pygame_gui.elements.UIHorizontalSlider(
+            relative_rect=pygame.Rect(
+                (self.screen_width // 2, self.screen_height // 2 * 0.9), (self.screen_width // 4, self.screen_height // 12)),
+            start_value=50.0,
+            value_range=(0.0, 100.0, 1.0),
+            manager=manager
+        )
+
+        self.volume_data = {"volume1": self.slider1.get_current_value()}
+
+        self.slider2 = pygame_gui.elements.UIHorizontalSlider(
+            relative_rect=pygame.Rect(
+                (self.screen_width // 2, self.screen_height // 2 * 1.2), (self.screen_width // 4, self.screen_height // 12)),
+            start_value=50.0,
+            value_range=(0.0, 100.0, 1.0),
+            manager=manager
+        )
+
+        self.slider3 = pygame_gui.elements.UIHorizontalSlider(
+            relative_rect=pygame.Rect(
+                (self.screen_width // 2, self.screen_height // 2 * 1.5), (self.screen_width // 4, self.screen_height // 12)),
+            start_value=50.0,
+            value_range=(0.0, 100.0, 1.0),
+            manager=manager
+        )
+
+        # 나가기버튼 생성
+        self.button_rect = pygame.Rect(
+            (self.screen_width // 2 * 0.75, self.screen_height // 2 * 1.8), (self.screen_width // 5, self.screen_height // 15))
+        self.exit_button = UIButton(
+            relative_rect=self.button_rect, text='Exit', manager=manager)
+
+
+    def handle_event(self, event):
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == self.exit_button:
+                # 순환 참조때문에 import 조금 늦게~ㅎ
+                from screens.setting_screen import SettingScreen
+                self.next_screen = SettingScreen
+                self.is_running = False
+
+         # run 함수
+    def run(self, events: list[Event]) -> bool:
+        
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.text, self.text_rect)
+        self.screen.blit(self.text1, self.text1_rect)
+        self.screen.blit(self.text2, self.text2_rect)
+        self.screen.blit(self.text3, self.text3_rect)
+
+        for event in events:
+            self.handle_event(event)
+
+        if self.networking.current_game.is_started:
+            self.is_running = False
+        return self.is_running
